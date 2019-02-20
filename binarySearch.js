cbdfcd5047d6bf131480c2577a6f235e56889b56
April Copes <antdevelopment1@gmail.com>
2019-02-19 19:21:31 -0500

    Adds a few comments

--- binarySearch.js
+++ binarySearch.js
@@ -1,3 +1,5 @@
+// Linear is the best we can do an unsorted array
+// Binary works great for sorted arrays
 // Write a function that accepts a sorted array and a value
 function binarySearch(arr, num) {
     // Create a left pointer at the start of the array and a right pointer at the end of the array
@@ -6,18 +8,18 @@ function binarySearch(arr, num) {
     // Create a pointer in the middle
     let middle = Math.floor((start + end) / 2);
     console.log(start, end, middle)
-// While the left pointer comes before the right pointer
-while (arr[middle] !== num && start <= end) {
-    // If you find the value want return the index
-    if (num < middle) {
-        end = middle - 1;
-    } else {
-        start = middle + 1
+    // While the left pointer comes before the right pointer
+    while (arr[middle] !== num && start <= end) {
+        // If you find the value want return the index
+        if (num < middle) {
+            end = middle - 1;
+        } else {
+            start = middle + 1
+        }
+        // If the value is too small move the left pointer up
+        // If the value if to high move the right pointer down
+        middle = Math.floor((start + end) / 2)
     }
-    // If the value is too small move the left pointer up
-    // If the value if to high move the right pointer down
-    middle = Math.floor((start + end) / 2)
-}
     // If we never find the value return -1
     return arr[middle] === num ? middle : -1
     
